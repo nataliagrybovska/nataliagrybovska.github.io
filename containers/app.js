e3c071767b6e364010f81756f707626cdcfc6951
Natalia Grybovska <natashagribowski@gmail.com>
2016-02-23 10:13:34 +0200

    Replaced the soundcloud with a telegram button

--- containers/app.js
+++ containers/app.js
@@ -5,7 +5,7 @@ var App = React.createClass({
         return (
             <div className="main-page page">
                 <div className="page-content">
-                    <div className="ui centered five column grid">
+                    <div className="ui centered four column grid">
                         <div className="centered column main-img">
                             <img className="ui medium circular image" src="/images/the-sea.jpg"/>
                         </div>
@@ -19,18 +19,18 @@ var App = React.createClass({
                                 </a>
                             </div>
                             <div className="column">
-                                <a href="http://instagram.com/natalia.grybovska/" target="_blank">
-                                    <i className="big instagram square grey icon link icon"/>
-                                </a>
-                            </div>
-                            <div className="column">
                                 <a href="http://www.facebook.com/natasha.gribowski" target="_blank">
                                     <i className="big facebook square grey icon link icon"/>
                                 </a>
                             </div>
                             <div className="column">
-                                <a href="http://soundcloud.com/natalia-grybovska" target="_blank">
-                                    <i className="big soundcloud grey square  icon link icon"/>
+                                <a href="http://instagram.com/natalia.grybovska/" target="_blank">
+                                    <i className="big instagram square grey icon link icon"/>
+                                </a>
+                            </div>
+                            <div className="column">
+                                <a href="https://telegram.me/NataliaGrybovska" target="_blank">
+                                    <i className="big send grey icon link icon"/>
                                 </a>
                             </div>
                             <div className="column">
